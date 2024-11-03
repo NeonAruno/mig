d8f30567baa6e77c1fe5132054677e90ce668b86
NeonAruno <neoncraft590@gmail.com>
2024-11-03 15:44:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,25 @@
-from flask import Flask
-import threading
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
-app = Flask(__name__)
 TOKEN = "7163922987:AAHxvBbBUmW_z_Ircu0CHR1plEK7yGey1jo"
 
-@app.route("/")
-def health_check():
-    return "Bot is running"
-async def start(up: Update,con: CallbackContext) -> None:
-	await up.message.reply_text("ok")
-def run_server():
-    app.run(host="0.0.0.0", port=8080)
 
-def run_bot():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-if __name__ == "__main__":
-    threading.Thread(target=run_server).start()
-    run_bot()
+app = Flask(__name__)
+app = ApplicationBuilder().token(TOKEN).build()
+
+@app.route('/webhook', methods=['POST'])
+async def webhook() -> str:
+    update = Update.de_json(request.get_json(force=True), app.bot)
+    await app.process_update(update)
+    return 'OK'
+
+@app.route('/setwebhook', methods=['GET'])
+def set_webhook():
+    app.bot.set_webhook(url='https://app.koyeb.com/webhook')
+    return 'Webhook set!'
+
+if __name__ == '__main__':
+    app.run(port=8443)
+	
