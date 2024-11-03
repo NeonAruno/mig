da2250701481f9e47392b6d9440d55bb8c0aec4a
NeonAruno <neoncraft590@gmail.com>
2024-11-03 15:52:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,42 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler,CallbackContext,ApplicationBuilder,MessageHandler,filters
 
 TOKEN = "7163922987:AAHxvBbBUmW_z_Ircu0CHR1plEK7yGey1jo"
 
+chats = [-1001922791146]
+async def start(up: Update,con: CallbackContext)-> None:
+	message = up.message
+	await message.reply_text("ok")
 
-from flask import Flask, request
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-app = Flask(__name__)
-app = ApplicationBuilder().token(TOKEN).build()
+async def handlerf(up: Update,con: CallbackContext)-> None:
+	message = up.channel_post
+	print(message.chat)
+	try:
+		if(message.photo and message.chat.id in chats):
+			photo = message.photo[-1].file_id
+			caption = f"<b>{message.caption}\n\n<i>#new_post</i></b>"
+			key = [
+					[InlineKeyboardButton("🍿 REGARDER 🍿",url=f"t.me/{message.chat.username}/{message.id}")],
+					[InlineKeyboardButton("⭐ Plus d'animer ⭐",url="https://t.me/+QtT1awr74l5iYWQ0")]
+				]
+			reply_markup = InlineKeyboardMarkup(key)
+			if "https://" not in caption:
+				await con.bot.send_photo(chat_id=-1001928904816,photo=photo,caption=caption,reply_markup=reply_markup,parse_mode="HTML")
+			#for chat in chats:
+			#	await con.bot.delete_message(chat_id=chat,message_id,=message.id)
+			#	
+	except Exception as e:
+		print(e)
 
-@app.route('/webhook', methods=['POST'])
-async def webhook() -> str:
-    update = Update.de_json(request.get_json(force=True), app.bot)
-    await app.process_update(update)
-    return 'OK'
 
-@app.route('/setwebhook', methods=['GET'])
-def set_webhook():
-    app.bot.set_webhook(url='https://app.koyeb.com/webhook')
-    return 'Webhook set!'
-
-if __name__ == '__main__':
-    app.run(port=8443)
+def main():
+	app = ApplicationBuilder().token(TOKEN).build()
+	app.add_handler(CommandHandler("start",start))
+	app.add_handler(MessageHandler(filters.ChatType.CHANNEL,handlerf))
+	print("Bot en marche")
+	app.run_polling()
 	
+
+if __name__ == "__main__":
+	main()
